50085222fde6d7e750b3172a0b4e7ca7bfbc8991
Viktor8 <wiktor4444@ukr.net>
2018-06-16 14:38:53 +0300

    UseDropdownMenuTreeInSprintsGreed

--- ScrumMaker/ScrumMaker/ClientApp/components/SprintsGrid.tsx
+++ ScrumMaker/ScrumMaker/ClientApp/components/SprintsGrid.tsx
@@ -2,46 +2,141 @@
 import { RouteComponentProps } from 'react-router';
 import 'isomorphic-fetch';
 
-interface ISprint {
-     id: number;
-     stage: string;
-     history: ISprintHistory
-     backlog: string;
-     defects: string;
-     dailyScrums: string;
-     review: string;
-     retrospective: string;
 
-    // public constructor(params: any)
-    // {
-    //     //super(params);
-    //     this.id = params.id;
-    //     this.stage = params.stage;
-    //     //this.history = params.history;
-    //     this.backlog = params.backlog;
-    //     this.defects = params.defects;
-    //     this.dailyScrums = params.deilyScrums;
-    //     this.review = params.review;
-    //     this.retrospective = params.retrospective;
-    // }
+class SprintHistory {
 
-    // public render()
-    // {
-    //     return <td>Hello</td>
+    public empty: boolean;
 
-    // }
+    public id: number = -1;
+    initiated: Date;
+    planned: Date;
+    begined: Date;
+    reviewDone: Date;
+    retrospectiveDone: Date;
+
+    constructor(params: any) {
+
+
+        if (params == null || params == undefined) {
+            this.empty = true;
+            return;
+        }
+        this.empty = false;
+        this.id = params.id;
+
+
+        this.initiated = new Date(params.initiated);
+        try {
+            this.planned = new Date(params.planned);
+            this.begined = new Date(params.begined);
+            this.reviewDone = new Date(params.reviewDone);
+            this.retrospectiveDone = new Date(params.retrospectiveDone);
+        } catch (e) { alert(e) }
+    }
+
+
+
+
+    public renderAsMenu() {
+
+        if (this.empty)
+            return <p>No Data</p>;
+        else
+            return <div className="dropdown">
+                <div id="{this.id}" role="button" data-toggle="dropdown" className="btn btn-primary" >
+                    History <span className="caret"></span>
+                </div>
+                <ul className="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+                    <li className="list-group-item"><pre>Initiated   {this.initiated.toLocaleDateString()}</pre></li>
+                    <li className="list-group-item"><pre>Planned     {this.planned.toLocaleDateString()}</pre></li>
+                    <li className="list-group-item"><pre>Beginned    {this.begined.toLocaleDateString()}</pre></li>
+                    <li className="list-group-item"><pre>Review      {this.reviewDone.toLocaleDateString()}</pre></li>
+                    <li className="list-group-item"><pre>Retrospective {this.retrospectiveDone.toLocaleDateString()}</pre></li>
+                </ul>
+            </div>
+    }
 }
-interface ISprintHistory
-{
-    id: number;
-    initiated: string;
-    planned: string;
-    begined: string;
-    reviewDone: string;
-    retrospectiveDone: string;
+
+class User {
+
+    empty: boolean = true;
+    constructor(params: any) {
+        if (params == null || params == undefined) {
+            return;
+        }
+
+        this.activity = params.activity;
+        this.login = params.login;
+        this.password = params.password;
+        this.roleId = params.roleId;
+        this.userId = params.userId;
+        this.empty = false;
+    }
+
+    activity: boolean = false;
+    login: string = "";
+    password: string = "";
+    roleId: number = -1;
+    userId: number = -1;
+
+    render() {
+        if (this.empty)
+            return "NoData";
+        return <li className="dropdown-submenu">
+            <div > {this.login} </div>
+            <ul className="dropdown-menu">
+                <li className="dropdown-item"><b><pre>UserID:  {this.userId}</pre></b>  </li>
+                <li className="dropdown-item"><b><pre>Login:   {this.login}</pre></b> </li>
+                <li className="dropdown-item"><b><pre>Password:{this.password}</pre></b> </li>
+                <li className="dropdown-item"><b><pre>Activity:{this.activity ? "true" : "false"}</pre></b> </li>
+                <li className="dropdown-item"><b><pre>RoleId:  {this.roleId}</pre></b> </li>
+            </ul>
+        </li>          
+    }
 }
 
-class Sprint{
+class Team {
+
+    empty: boolean = true;
+    name: string = "";
+    id: number = -1;
+    members: User[] = [];
+
+    constructor(params: any) {
+        if (params == null || params == undefined) {
+            return;
+        }
+        this.name = params.name;
+        this.id = params.id;
+        this.empty = false;
+
+        //var members = params.members.map( u => new User(u));
+        var members = [];
+        for (var i = 0; i < params.members.length; i++) {
+            members[i] = new User(params.members[i]);
+
+        }
+
+        this.members = members;
+    }
+
+
+
+    renderAsMenu() {
+        if (this.empty)
+            return "NoData"
+        return <div className="dropdown">
+            <div id="{this.id}" role="button" data-toggle="dropdown" className="btn btn-primary" >
+                {this.name} <span className="caret"></span>
+            </div>
+            <ul className="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+                {this.members.map(u => u.render())}
+            </ul>
+        </div>
+    }
+}
+
+class Sprint {
     id: number;
     stage: string;
     history: SprintHistory
@@ -50,75 +145,34 @@ class Sprint{
     dailyScrums: string;
     review: string;
     retrospective: string;
+    team: Team;
 
-   public constructor(params: ISprint)
-   {
-       //super(params);
-       this.id = params.id;
-       this.stage = params.stage;
-       this.history = new SprintHistory(params.history);
-       this.backlog = params.backlog;
-       this.defects = params.defects;
-       this.dailyScrums = params.dailyScrums;
-       this.review = params.review;
-       this.retrospective = params.retrospective;
-   }
-
-    public renderAsTableRow()
-    {
-        return (<tr key={this.id}>
-                     <td>{this.id}</td>
-                     <td>{this.stage}</td>
-                     <td>{this.review}</td>
-                     <td>{this.history.renderAsList()}</td>
-                     <td>{this.retrospective}</td>
-                 </tr>);
-              
-
-    }
-}
-
-class SprintHistory{
-    constructor(params: ISprintHistory) {
-        if (params == null || params == undefined)
-        {
-            this.empty = true;
-            return;
-        }
-        this.empty = false;
+    public constructor(params: any) {
         this.id = params.id;
-        this.initiated = params.initiated;
-        this.planned = params.planned;
-        this.begined = params.begined;
-        this.reviewDone = params.reviewDone;
-        this.retrospectiveDone = params.retrospectiveDone;
+        this.stage = params.stage;
+        this.history = new SprintHistory(params.history);
+        this.backlog = params.backlog;
+        this.defects = params.defects;
+        this.dailyScrums = params.dailyScrums;
+        this.review = params.review;
+        this.retrospective = params.retrospective;
+        this.team = new Team(params.team);
     }
 
-    empty: boolean;
-
-    public id :number;
-    public initiated: string;
-    public planned: string;
-    public begined: string;
-    public reviewDone: string;
-    public retrospectiveDone: string;
-
-    public renderAsList() {
-
-        if (this.empty)
-            return <p>No Data</p>;
-        else
-            return (
-            <ul>
-                <li>Initiated   {this.initiated}</li>
-                <li>Planned     {this.planned}</li>
-                <li>Beginned    {this.begined}</li>
-                <li>Reviev      {this.reviewDone}</li>
-                <li>Retrospective {this.retrospectiveDone}</li>
-            </ul>);
+    public renderAsTableRow() {
+        return <tr key={this.id}>
+            <td>{this.id}</td>
+            <td>{this.team.renderAsMenu()}</td>
+            <td>{this.stage}</td>
+            <td>{this.review}</td>
+            <td>{this.history.renderAsMenu()}</td>
+            <td>{this.retrospective}</td>
+        </tr>;
 
     }
 }
+
+
 
 
 interface SprintDataFetchingState {
@@ -159,6 +213,7 @@ export class SprintsGrid extends React.Component<RouteComponentProps<{}>, Sprint
             <thead>
                 <tr>
                     <th>Database ID</th>
+                    <th>Team</th>
                     <th>Stage</th>
                     <th>Review</th>
                     <th>History</th>
@@ -166,16 +221,7 @@ export class SprintsGrid extends React.Component<RouteComponentProps<{}>, Sprint
                 </tr>
             </thead>
             <tbody>
-                {sprints.map(sprint =>
-                    // <tr key={sprint.id}>
-                    //     <td>{sprint.id}</td>
-                    //     <td>{sprint.stage}</td>
-                    //     <td>{sprint.review}</td>
-                    //     <td>{sprint.history}</td>
-                    //     <td>{sprint.retrospective}</td>
-                    // </tr>
-                    sprint.renderAsTableRow()
-                )}
+                {sprints.map(sprint => sprint.renderAsTableRow())}
             </tbody>
         </table>;
     }
