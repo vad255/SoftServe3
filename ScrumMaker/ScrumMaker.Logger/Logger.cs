7158f318403079262872c448065fabfab275a2a1
Obrizan Kateryna <Katia_Proz@ukr.net>
2018-06-18 23:33:16 +0300

    Implemented logger using NLog

--- ScrumMaker/ScrumMaker.Logger/Logger.cs
+++ ScrumMaker/ScrumMaker.Logger/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.IO;
 using System.Reflection;
 using System.Xml;
@@ -7,18 +8,7 @@ namespace ScrumMaker.Logger
 {
     public class Logger
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Logger));
-
-        static Logger()
-        {
-            XmlDocument log4netConfig = new XmlDocument();
-            log4netConfig.Load(File.OpenRead(Path.Combine(GetExecutenDirectory(), "log4net.config")));
-
-            var repo = log4net.LogManager.CreateRepository(
-                Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
-
-            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
-        }
+        private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
 
         public static void LogFatal(string massage)
         {
@@ -43,14 +33,6 @@ namespace ScrumMaker.Logger
         public static void LogDebug(string massage)
         {
             log.Debug(massage);
-        }
-
-        private static string GetExecutenDirectory()
-        {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            return Path.GetDirectoryName(path);
         }
     }
 }
