efd0661c7f5035fba1613dd8cd1b70d86f2b4dea
Viktor8 <wiktor4444@ukr.net>
2018-06-27 14:53:09 +0300

    EnumWork

--- ScrumMaker/ScrumMaker/ClientApp/components/Fliters/EnumFilter.tsx
+++ ScrumMaker/ScrumMaker/ClientApp/components/Fliters/EnumFilter.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Filter, IFilterConfiguration } from './Filter'
+import { FormControl } from 'react-bootstrap';
 
 export interface IEnumFilterConfiguration extends IFilterConfiguration {
-    enumType: {};
+    enumType: any;
 }
 
 export class EnumFilter extends Filter {
@@ -11,23 +12,61 @@ export class EnumFilter extends Filter {
         this.enumType = params.enumType;
     }
 
-    enumType: {};
+    enumType: {} = [];
     filteringString: string = '';
 
     private OnChangeHandler(e: any) {
 
-        console.log(this.enumType);
+        this.filteringString = "";
 
-        this.filteringString = 'contains(cast(' + this.state.filterKey + ', \'Edm.String\'), \'' + e.target.value + '\')'
-        this.state.onFilterChanged(this.state.filterKey, this.filteringString);
+        for (let i = 0; i < e.target.options.length; i++) {
+
+            let enumVal = e.target.options[i]
+
+
+            if (!enumVal.selected)
+                continue;
+
+            if (this.filteringString.length != 0)
+                this.filteringString += ' or '
+
+            this.filteringString +=
+                this.state.filterKey.toString() +' eq \'' +
+                enumVal.value.toString() +
+                '\'';
+        }
+        this.state.onFilterChanged(this.state.filterKey, '(' + this.filteringString + ')');
     }
 
     public render() {
-        return <select className="form-control" multiple={true}>
-        <option>Mustard</option>
-        <option>Ketchup</option>
-        <option>Relish</option>
-      </select>
-      
+        return <div className="dropdown">
+            <div data-toggle="dropdown" >
+                Selector<span className="caret"></span>
+            </div>
+            {this.renderSelect()}
+        </div>
+
+
+
+    }
+
+    private renderSelect() {
+        let names: string[] = [];
+        for (let iterator in this.enumType) {
+            if (!parseInt(iterator))
+                names.push(iterator.toString());
+        }
+
+        let items: JSX.Element[] = [];
+        for (var i = 0; i < names.length; i++) {
+            items.push(<option key={i + 1} value={names[i]}>{names[i]}</option>);
+        }
+
+
+        return <select className="dropdown-menu autooverflow" multiple={true}
+            onChange={this.OnChangeHandler.bind(this)}>
+            {items}
+        </select>
+
     }
 }
