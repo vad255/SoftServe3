6dffba0f3b5b62ac6bf01fada7d3e1f4477629bc
Roman Prystaiko <prystaiko.roman@gmail.com>
2018-06-29 18:01:13 +0300

    Some Changes

---
+++ ScrumMaker/ScrumMaker/ClientApp/components/Error.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Link } from 'react-router-dom';
+
+export class Error extends React.Component<RouteComponentProps<any>, {}> {
+
+    public render() {
+        return <div className="text-center">
+                   <img src="img/something-went-wrong.gif" />
+                   <h1>Go to <Link to="/">Home page</Link></h1>
+               </div>;
+    }
+}
