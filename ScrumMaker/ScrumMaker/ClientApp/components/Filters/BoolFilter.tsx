a0c15bf3ef874122858cdc2e749cf44381c96c8b
Dmytro Khomyk <dimaxomuk005@gmail.com>
2018-06-29 15:49:42 +0300

    Add filter
    add filter for usergrid and some changes with authentication

---
+++ ScrumMaker/ScrumMaker/ClientApp/components/Filters/BoolFilter.tsx
@@ -0,0 +1,27 @@
+﻿import * as React from 'react';
+import { Filter, IFilterConfiguration } from './Filter'
+
+export class BoolFilter extends Filter {
+    public Reset(): void {
+        throw new Error("Method not implemented.");
+    }
+    constructor(params: IFilterConfiguration) {
+        super(params);
+    }
+
+    filteringString: string = '';
+
+    private OnChangeHandler(e: any) {
+        var temp = -1;
+        if (e.target.value === "false")
+            temp = 0;
+        else if (e.target.value === "true")
+            temp = 1;
+        this.filteringString = 'contains(cast(' + this.state.filterKey + ', \'Edm.String\'), \'' + temp + '\')'
+        this.state.onFilterChanged(this.state.filterKey, this.filteringString);
+    }
+
+    public render() {
+        return <input type="text" onChange={((e: any) => this.OnChangeHandler(e)).bind(this)} />
+    }
+}
--- ScrumMaker/ScrumMaker/Startup.cs
+++ ScrumMaker/ScrumMaker/Startup.cs
@@ -62,7 +62,7 @@ namespace ScrumMaker
                 };
             });
 
-            string connectionStr = Configuration.GetConnectionString("Kateryna");
+            string connectionStr = Configuration.GetConnectionString("Dmytro");
 
 
             services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionStr, b => b.UseRowNumberForPaging()));
