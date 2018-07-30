6bb823cb444ab9eec24e485f8b1c660de939f222
Obrizan Kateryna <Katia_Proz@ukr.net>
2018-07-30 14:33:42 +0300

    Deleted retrospective from sprint grid. Changes styles in feature edit page.

--- ScrumMaker/ScrumMaker/ClientApp/components/Grids/SprintsGrid.tsx
+++ ScrumMaker/ScrumMaker/ClientApp/components/Grids/SprintsGrid.tsx
@@ -35,9 +35,8 @@ export class SprintsGrid extends Grid {
             <th className="well menu_links col-md-1" onClick={() => this.OrderBy("name")}>Name</th>
             <th className="well menu_links col-md-1" onClick={() => this.OrderBy("team")}>Team</th>
             <th className="well menu_links col-md-1" onClick={() => this.OrderBy("stage")}>Stage</th>
-            <th className="well menu_links col-md-1" onClick={() => this.OrderBy("review")}>Review</th>
+            <th className="well menu_links col-md-1" onClick={() => this.OrderBy("review")}>Goal</th>
             <th className="well menu_links col-md-1" onClick={() => this.OrderBy("history")}>History</th>
-            <th className="well menu_links col-md-1" onClick={() => this.OrderBy("retrospective")}>Retrospective</th>
             <th className="well menu_links col-md-1">
                 <div onClick={this.FilterButtonClick.bind(this)}>
                     <span className="nowrap">Show Filters<span className="caret"></span></span>
@@ -53,8 +52,7 @@ export class SprintsGrid extends Grid {
             new TextFilter({ filterKey: "team/name" }),
             new EnumFilter({ filterKey: "stage", enumType: SprintStage }),
             new TextFilter({ filterKey: "review" }),
-            new IntFilter({ filterKey: "history/begined" }),
-            new TextFilter({ filterKey: "retrospective" })
+            new IntFilter({ filterKey: "history/begined" })
         ]
 
         return <FiltersManager
