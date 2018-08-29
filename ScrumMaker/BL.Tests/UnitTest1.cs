2583f22bd6b9e9bf86941af2584abdbcac5d9f09
Roman Prystaiko <prystaiko.roman@gmail.com>
2018-08-29 16:45:14 +0300

    commenting unittests

--- ScrumMaker/BL.Tests/UnitTest1.cs
+++ ScrumMaker/BL.Tests/UnitTest1.cs
@@ -20,97 +20,97 @@ using System.CodeDom;
 
 namespace BL.Tests
 {
-    [TestFixture]
-    public class TestTesting
-    {
-        const string connectionString =
-              "Server=.\\SQLEXPRESS;Database=ScrumMaker;Trusted_Connection=True;MultipleActiveResultSets=true";
+    //[TestFixture]
+    //public class TestTesting
+    //{
+    //    const string connectionString =
+    //          "Server=.\\SQLEXPRESS;Database=ScrumMaker;Trusted_Connection=True;MultipleActiveResultSets=true";
 
-        static DbContextOptions options = new DbContextOptionsBuilder().UseSqlServer(connectionString).Options;
-        //static DbContext cont = new DbContext(options);
+    //    static DbContextOptions options = new DbContextOptionsBuilder().UseSqlServer(connectionString).Options;
+    //    //static DbContext cont = new DbContext(options);
 
-        static DataContext context = new DataContext(options);
+    //    static DataContext context = new DataContext(options);
 
-        //static var uow = new Mock<IUnitOfWork(cont)>;
-        //static IStoriesManager _manager = new StoriesManager(uow);
-        //static IRepository<Story> _stories = new Repository<Story>(cont);
-        private readonly Mock<IStoriesManager> _manager = new Mock<IStoriesManager>();
-        private readonly Mock<IRepository<Story>> _stories = new Mock<IRepository<Story>>();
+    //    //static var uow = new Mock<IUnitOfWork(cont)>;
+    //    //static IStoriesManager _manager = new StoriesManager(uow);
+    //    //static IRepository<Story> _stories = new Repository<Story>(cont);
+    //    private readonly Mock<IStoriesManager> _manager = new Mock<IStoriesManager>();
+    //    private readonly Mock<IRepository<Story>> _stories = new Mock<IRepository<Story>>();
 
-        [Test]
-        public void TestStoriesView()
-        {
-            _stories.Setup(m => m.GetAll()).Returns(new[] {
-                new Story (),
-                new Story(),
-                new Story (),
-                new Story ()
-            }.AsQueryable());
+    //    [Test]
+    //    public void TestStoriesView()
+    //    {
+    //        _stories.Setup(m => m.GetAll()).Returns(new[] {
+    //            new Story (),
+    //            new Story(),
+    //            new Story (),
+    //            new Story ()
+    //        }.AsQueryable());
 
-            var controller = new StoriesController(context, _manager.Object, _stories.Object);
-            var response = controller.Get();
+    //        var controller = new StoriesController(context, _manager.Object, _stories.Object);
+    //        var response = controller.Get();
             
      
-            Assert.AreEqual(new OkObjectResult(_stories.Object.GetAll()), response as OkObjectResult);
-        }
+    //        Assert.AreEqual(new OkObjectResult(_stories.Object.GetAll()), response as OkObjectResult);
+    //    }
 
-        [Test]
-        public void TestAddNewStoryEntity()
-        {
-            Story story = new Story()
-            {
-                Name = "TestStory",
-                Description = "TestDescription",
-                SprintId = 1,
-                FeatureId = 2,
-                TeamId = 3,
-                UserId = 4,
-                Status = StoryStatus.PendingApproval,
-                Defects = new List<Defect>(),
-                Tasks = new List<ScrumTask>()
-            };
+    //    [Test]
+    //    public void TestAddNewStoryEntity()
+    //    {
+    //        Story story = new Story()
+    //        {
+    //            Name = "TestStory",
+    //            Description = "TestDescription",
+    //            SprintId = 1,
+    //            FeatureId = 2,
+    //            TeamId = 3,
+    //            UserId = 4,
+    //            Status = StoryStatus.PendingApproval,
+    //            Defects = new List<Defect>(),
+    //            Tasks = new List<ScrumTask>()
+    //        };
 
-            var controller = new StoriesController(context, _manager.Object, _stories.Object);
-            var response = controller.Post(story);
+    //        var controller = new StoriesController(context, _manager.Object, _stories.Object);
+    //        var response = controller.Post(story);
 
-            CreatedODataResult<Story> result = new CreatedODataResult<Story>(story);
-            var res = response as CreatedODataResult<Story>;
-            Assert.AreEqual(result, res);
-        }
+    //        CreatedODataResult<Story> result = new CreatedODataResult<Story>(story);
+    //        var res = response as CreatedODataResult<Story>;
+    //        Assert.AreEqual(result, res);
+    //    }
 
-        [Test]
-        public void TestDeleteStoryEntityById()
-        {
-            var controller = new StoriesController(context, _manager.Object, _stories.Object);
-            var response = controller.Delete(14);
-            Assert.AreEqual(false, response);
-            var response2 = controller.Delete(130000);
-            Assert.AreEqual(true, response2);
-        }
+    //    [Test]
+    //    public void TestDeleteStoryEntityById()
+    //    {
+    //        var controller = new StoriesController(context, _manager.Object, _stories.Object);
+    //        var response = controller.Delete(14);
+    //        Assert.AreEqual(false, response);
+    //        var response2 = controller.Delete(130000);
+    //        Assert.AreEqual(true, response2);
+    //    }
 
-        [Test]
-        public void TestUpdateStoryEntity()
-        {
-            Story story = new Story()
-            {
-                Name = "TestStory",
-                Description = "TestDescription",
-                SprintId = 1,
-                FeatureId = 2,
-                TeamId = 3,
-                UserId = 4,
-                Status = StoryStatus.PendingApproval,
-                Defects = new List<Defect>(),
-                Tasks = new List<ScrumTask>()
-            };
+    //    [Test]
+    //    public void TestUpdateStoryEntity()
+    //    {
+    //        Story story = new Story()
+    //        {
+    //            Name = "TestStory",
+    //            Description = "TestDescription",
+    //            SprintId = 1,
+    //            FeatureId = 2,
+    //            TeamId = 3,
+    //            UserId = 4,
+    //            Status = StoryStatus.PendingApproval,
+    //            Defects = new List<Defect>(),
+    //            Tasks = new List<ScrumTask>()
+    //        };
 
-            Delta<Story> delta = new Delta<Story>();
+    //        Delta<Story> delta = new Delta<Story>();
 
-            var controller = new StoriesController(context, _manager.Object, _stories.Object);
-            var response = controller.Patch(12, delta);
-            UpdatedODataResult<Story> result = new UpdatedODataResult<Story>(story);
+    //        var controller = new StoriesController(context, _manager.Object, _stories.Object);
+    //        var response = controller.Patch(12, delta);
+    //        UpdatedODataResult<Story> result = new UpdatedODataResult<Story>(story);
 
-            Assert.AreEqual(result, response as UpdatedODataResult<Story>);
-        }
-    }
+    //        Assert.AreEqual(result, response as UpdatedODataResult<Story>);
+    //    }
+    //}
 }
