b29dc529ae147be1c26bf62ececfdfe9b1ca0d15
mykhalchuk18 <40005510+mykhalchuk18@users.noreply.github.com>
2018-08-30 13:51:13 +0300

    Validation, deleting, unit tests and other changes
    implementing validation on empty fields, checking if deleted, unit tests for task controller and other changes

--- ScrumMaker/BL.Tests/TaskControllerTest.cs
+++ ScrumMaker/BL.Tests/TaskControllerTest.cs
@@ -11,15 +11,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 
-
-
 namespace UnitTests
 {
     [TestClass]
     public class TaskControllerTest
     {
         [TestMethod]
-        public void GetTasks_ReturnsOk()
+        public void GetTasks_ReturnsOkObjectResult()
         {
             // arrange
             var mockRepository = new Mock<IRepository<ScrumTask>>();
@@ -35,14 +33,14 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void DeleteTasks_ReturnsBool()
+        public void DeleteTasks_ReturnsBoolean()
         {
             // Arrange
             var mockRepository = new Mock<IRepository<ScrumTask>>();
             var controller = new TasksController(mockRepository.Object);
-
+            
             // Act
-            var boolResult = controller.Delete(24);
+            var boolResult = controller.Delete(5);
 
             // Assert
             Assert.IsNotNull(boolResult);
@@ -84,29 +82,28 @@ namespace UnitTests
             Assert.IsInstanceOfType(actionResult, typeof(CreatedODataResult<ScrumTask>));
         }
 
-        [TestMethod]
-        public void PatchTasks_ReturnsUpdatedODataResult()
-        {
-            // Arrange
-            var mockRepository = new Mock<IRepository<ScrumTask>>();
-            var controller = new TasksController(mockRepository.Object);
+        //[TestMethod]
+        //public void PatchTasks_ReturnsUpdatedODataResult()
+        //{
+        //    // Arrange
+        //    var mockRepository = new Mock<IRepository<ScrumTask>>();
+        //    var controller = new TasksController(mockRepository.Object);
 
-            var delta = new Delta<ScrumTask>(typeof(ScrumTask));
-            //delta.TrySetPropertyValue(nameof(ScrumTask.TaskId), 4);
-            //delta.TrySetPropertyValue(nameof(ScrumTask.Summary), "Johanson");
+        //    var delta = new Delta<ScrumTask>(typeof(ScrumTask));
+        //    //delta.TrySetPropertyValue(nameof(ScrumTask.TaskId), 4);
+        //    //delta.TrySetPropertyValue(nameof(ScrumTask.Summary), "Johanson");
 
 
-            //act
-            var actionResult = controller.Patch(4, delta);
-            Assert.IsNotNull(actionResult);
+        //    //act
+        //    var actionResult = controller.Patch(4, delta);
+        //    Assert.IsNotNull(actionResult);
 
-            var updatedResult = actionResult as UpdatedODataResult<ScrumTask>;
-            Assert.IsNotNull(updatedResult);
-            //delta.TrySetPropertyValue(nameof(ScrumTask.TaskId), 4);
-            //delta.TrySetPropertyValue(nameof(ScrumTask.Summary), "Johanson");
-            //assert
-            //Assert.IsInstanceOfType(actionResult, typeof(UpdatedODataResult<ScrumTask>));
-
-        }
-        }
+        //    var updatedResult = actionResult as UpdatedODataResult<ScrumTask>;
+        //    Assert.IsNotNull(updatedResult);
+        //    //delta.TrySetPropertyValue(nameof(ScrumTask.TaskId), 4);
+        //    //delta.TrySetPropertyValue(nameof(ScrumTask.Summary), "Johanson");
+        //    //assert
+        //    //Assert.IsInstanceOfType(actionResult, typeof(UpdatedODataResult<ScrumTask>));
+        //}
     }
+}
